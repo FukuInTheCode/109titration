05e596fa339884a82e266e0726526911464c5185
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-03 23:34:16 +0200

    [refactor] -->  if the file have more than one semicolon, the program return 84

--- src/error_handling/check_file.py
+++ src/error_handling/check_file.py
@@ -6,7 +6,10 @@ def check_file(argv: list[str]) -> int:
         reader = csv.reader(file)
         for row in reader:
             line = row[0]
+            semicolon_count = line.count(";")
+            if semicolon_count != 1:
+                exit(84)
             semicolon_index = line.find(";")
-            if semicolon_index == -1 or semicolon_index == 0 or semicolon_index == len(line) - 1:
+            if semicolon_index == 0 or semicolon_index == len(line) - 1:
                 exit(84)
     return 0
