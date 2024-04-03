1ec97f0f3e498be815d64fed41df38a9a1bde9f2
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-03 23:31:32 +0200

    [refactor] -->  add the verification if the line inside the file doesnt have 2 number, the program return 84

---
+++ src/error_handling/check_file.py
@@ -0,0 +1,12 @@
+import csv
+
+
+def check_file(argv: list[str]) -> int:
+    with open(argv[1], "r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            line = row[0]
+            semicolon_index = line.find(";")
+            if semicolon_index == -1 or semicolon_index == 0 or semicolon_index == len(line) - 1:
+                exit(84)
+    return 0
