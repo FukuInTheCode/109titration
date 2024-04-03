15264cea712610242c295e4c080494b7805f2ac8
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-03 20:51:45 +0200

    [refactor] -->  call the function for check inside the file

--- src/error_handling/check_argv.py
+++ src/error_handling/check_argv.py
@@ -1,4 +1,5 @@
 from src.help.display_help import display_help
+from src.error_handling.check_file import check_file
 
 
 def check_argv(argv: list[str]) -> int:
@@ -6,3 +7,4 @@ def check_argv(argv: list[str]) -> int:
         exit(84)
     if argv[1] == "-h":
         return display_help()
+    return check_file(argv)
