2a72ed523396dac8e48328eeb61ebaa7930f9761
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-04 00:02:50 +0200

    [refactor] -->  if in the file, it's not a number, the program return 84

--- src/error_handling/check_file.py
+++ src/error_handling/check_file.py
@@ -1,6 +1,14 @@
 import csv
 
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
 def check_file(argv: list[str]) -> int:
     with open(argv[1], "r") as file:
         reader = csv.reader(file)
@@ -11,5 +19,9 @@ def check_file(argv: list[str]) -> int:
                 exit(84)
             semicolon_index = line.find(";")
             if semicolon_index == 0 or semicolon_index == len(line) - 1:
+                exit(84)
+            before_semicolon = line[:semicolon_index]
+            after_semicolon = line[semicolon_index + 1:]
+            if not is_number(before_semicolon) or not is_number(after_semicolon):
                 exit(84)
     return 0
