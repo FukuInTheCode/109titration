d7c5edc626af25fc837d5b47260c60751512c053
EmmaFrx <emma.farineaux@epitech.eu>
2024-04-05 11:43:00 +0200

    [feat] -->  add the function for print the derivatives

---
+++ src/print/print_derivatives.py
@@ -0,0 +1,5 @@
+def print_derivatives(values: list[list[float, float]]) -> int:
+    print("Derivative:")
+    for value in values:
+        print(f"{value[0]:.1f} ml -> {value[1]:.2f}")
+    return 0
