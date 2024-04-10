0984809f7c3c90bc3223c89a38f1328f5f25f266
Tom C <tom.carlier@epitech.eu>
2024-04-10 17:00:06 +0200

    [refactor] -->  update file(s)

--- src/derivatives/calc_derivative.py
+++ src/derivatives/calc_derivative.py
@@ -2,14 +2,15 @@ def calc_derivative(coords: list[tuple[float, float]]) -> list[tuple[float, floa
     der = []
 
     for i in range(1, len(coords) - 1, 1):
+        alpha = (coords[i][0] - coords[i - 1][0]) / (coords[i + 1][0] - coords[i - 1][0])
+        beta = (coords[i + 1][0] - coords[i][0]) / (coords[i + 1][0] - coords[i - 1][0])
         der.append(
             (
                 coords[i][0],
                 (
-                    (coords[i + 1][1] - coords[i][1]) / (coords[i + 1][0] - coords[i][0])
-                    + (coords[i][1] - coords[i - 1][1]) / (coords[i][0] - coords[i - 1][0])
-                )
-                / (coords[i + 1][0] - coords[i - 1][0]),
+                    alpha * (coords[i + 1][1] - coords[i][1]) / (coords[i + 1][0] - coords[i][0])
+                    + beta * (coords[i][1] - coords[i - 1][1]) / (coords[i][0] - coords[i - 1][0])
+                ),
             )
         )
     return der
